351217d680a1537577aaaac004ad0276dc670b4d
HersheyTheFluff <92188574+HersheyTheFluff@users.noreply.github.com>
2022-02-24 13:15:46 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@
           return w.state.hired;
         }).reduce(function(a, b){return a + b}, 0));
       }
-    }, 1000);
+    }, 50);
   }]);
 
   app.controller('ResearchController', ['$compile', function($compile) {
